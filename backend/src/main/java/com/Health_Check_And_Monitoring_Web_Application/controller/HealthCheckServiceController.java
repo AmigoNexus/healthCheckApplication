62f45241aa05123e8cb7b4126a54e1a5395774e7
ajitundareamigonexus <ajit.undare@amigonexus.com>
2026-02-24 12:50:13 +0530

    cors error fixed

--- backend/src/main/java/com/Health_Check_And_Monitoring_Web_Application/controller/HealthCheckServiceController.java
+++ backend/src/main/java/com/Health_Check_And_Monitoring_Web_Application/controller/HealthCheckServiceController.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/service")
-@CrossOrigin(origins = "${cors.allowed-origin:http://localhost:5173}")
 public class HealthCheckServiceController {
 
     @Autowired
--- backend/src/main/java/com/Health_Check_And_Monitoring_Web_Application/controller/PingController.java
+++ backend/src/main/java/com/Health_Check_And_Monitoring_Web_Application/controller/PingController.java
@@ -11,7 +11,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "${cors.allowed-origin:http://localhost:5173}")
 public class PingController {
 
     @Autowired
